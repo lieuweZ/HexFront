d52ca69b31d3413a4953fa5abce09618823a3973
Nicolaas van de meent junior <nicolaas.van.de.meent.junior@hva.nl>
2025-04-17 14:02:00 +0200

    #3 &Added File $Added Objectmanager file and changed begin state to GAME_STATE.

---
+++ game/Managers/ObjectManager.cs
@@ -0,0 +1,11 @@
+using System;
+using Blok3Game.Engine.Helpers;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Blok3Game.Engine.GameObjects
+{
+	public class PieceObject
+    {
+    }
+}
